168119db4dcbcb1827f7518e52f5859eda83618c
Locyst <154724542+Locyst@users.noreply.github.com>
2024-08-04 15:49:59 -0600

    Update select.py
    updated to 0.0.2 code

--- select.py
+++ select.py
@@ -18,26 +18,42 @@ class Option(object):
     return self.name
 
 class Selection(object):
-  def __init__(self, title, options, *, selected_symbol='->', unselected_symbol='  ',
-                multiselect=False, min_multiselect=0, multiselect_cursor_symbol='>',
-               multiselect_selected_symbol='●', multiselect_unselected_symbol='○',
-              defaulted_index=0):
+  def __init__(self, title, options,
+                 cursor_settings=None,
+                 selection_symbols=None,
+                 multiselect_settings=None,
+                 display_settings=None,
+                 defaulted_index=0,
+                 numbered_options=False,
+                 pick_through_number=False):
+
+    cursor_settings = cursor_settings or {}
+    selection_symbols = selection_symbols or {}
+    multiselect_settings = multiselect_settings or {}
+    display_settings = display_settings or {}
+
     self.title = title
     if isinstance(options, dict): self.options = [Option(name, call_name) for name, call_name in options.items()]
     elif isinstance(options, list): self.options = [Option(name, name) for name in options]
 
-    self.selected_symbol = selected_symbol
-    self.unselected_symbol = unselected_symbol
+    self.selected_symbol = selection_symbols.get('selected_symbol', '->')
+    self.unselected_symbol = selection_symbols.get('unselected_symbol', '  ')
 
-    self.multiselect_cursor_symbol = multiselect_cursor_symbol
-    self.multiselect_selected_symbol = multiselect_selected_symbol
-    self.multiselect_unselected_symbol = multiselect_unselected_symbol
+    self.multiselect_cursor_symbol = multiselect_settings.get('cursor_symbol', '>')
+    self.multiselect_selected_symbol = multiselect_settings.get('selected_symbol', '●')
+    self.multiselect_unselected_symbol = multiselect_settings.get('unselected_symbol', '○')
 
-    self.multiselect = multiselect
-    self.min_multiselect = min_multiselect
+    self.multiselect = multiselect_settings.get('enabled', False)
+    self.min_multiselect = multiselect_settings.get('min', 0)
+
+    self.multiselect_text = display_settings.get('multiselect_text', "\n   Use the space bar to select multiple entries\n\n")
 
     self.cursor = defaulted_index
+    self._normalize_cursor
     self.selected_indexs = []
+
+    self.numbered_options = numbered_options
+    self.pick_through_number = pick_through_number
 
   def _selectedcheck(self, idx):
     symbol = ''
@@ -54,35 +70,52 @@ class Selection(object):
 
     return symbol
 
+  def _normalize_cursor(self):
+    self.cursor = max(0, min(self.cursor, len(self.options) - 1))
 
   def _move_cursor(self, amount):
     self.cursor += amount
-    self.cursor = max(0, min(self.cursor, len(self.options) - 1))
+    self._normalize_cursor()
 
   def _draw(self):
     message = str()
     if self.title: message += f"\n   {self.title}\n\n"
-    if self.multiselect: message += "\n   Use the space bar to select multiple entries\n\n"
+    if self.multiselect: message += self.multiselect_text
     for idx, option in enumerate(self.options):
-      message += f'{self._selectedcheck(idx)} ' + repr(option) + '\n\n'
+      if self.numbered_options:
+        message += f'{idx} {self._selectedcheck(idx)} ' + repr(option) + '\n\n'
+      else:
+        message += f'{self._selectedcheck(idx)} ' + repr(option) + '\n\n'
     return message
+
+  def _clear(self):
+    os.system('clear' if os.name == 'posix' else 'cls')
 
   def run(self):
     while True:
-      os.system('clear' if os.name == 'posix' else 'cls')
+      self._clear()
       print(self._draw())
-      move = input("> ")
-      if move == UP_ARROW:
+      USER_INPUT = input("> ")
+      if USER_INPUT == UP_ARROW:
         self._move_cursor(-1)
-      elif move == DOWN_ARROW:
+
+      elif USER_INPUT == DOWN_ARROW:
         self._move_cursor(1)
-      elif " " in move:
-        if self.multiselect:
-          self.selected_indexs.append(self.cursor)
-      elif not move:
+
+      elif (" " in USER_INPUT) and (self.multiselect):
+        self.selected_indexs.append(self.cursor)
+
+      elif (self.pick_through_number) and (USER_INPUT.isnumeric()):
+        if 0 <= int(USER_INPUT) <= len(self.options) + 1:
+          if (self.multiselect):
+            self.selected_indexs.append(USER_INPUT)
+          else:
+            return {"OPTION_SELECTED": repr(self.options[int(USER_INPUT)]),
+                                     "INDEX": int(USER_INPUT)}
+      elif not USER_INPUT:
         break
 
-    os.system('clear' if os.name == 'posix' else 'cls')
+    self._clear()
     self._draw()
     if not self.multiselect:
       return {"OPTION_SELECTED": repr(self.options[self.cursor]),
